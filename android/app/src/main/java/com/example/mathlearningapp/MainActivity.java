c26dd35c125bf2a63c7426d1807b08b50d35afc5
Your Name <you@example.com>
2020-06-05 21:24:30 +0545

    initial

---
+++ android/app/src/main/java/com/example/mathlearningapp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.mathlearningapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,21 @@
+import 'package:mathlearningapp/models/user.dart';
+import 'package:mathlearningapp/screens/authenticate/authenticate.dart';
+import 'package:mathlearningapp/screens/home/home.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final user = Provider.of<User>(context);
+    
+    // return either the Home or Authenticate widget
+    if (user == null){
+      return Authenticate();
+    } else {
+      return Home();
+    }
+    
+  }
+}
